a09e1522a9bc59aa14ea65e32e11266da1aaa54a
Satayjit Biswas <61390425+Satayjit-Biswas@users.noreply.github.com>
2022-05-02 17:23:32 +0600

    first commit

---
+++ Basic/add_number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int num1,num2,total;
+    cout<<"Enter Two Number";
+    cin>>num1>>num2;
+    total=num1+num2;
+    cout<<"Total number = "<<total;
+}
---
+++ Basic/input.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter ID number :";
+    cin >> num;
+    cout<<"Your ID = "<<num;
+    cout<<"Enter Your Name :";
+    char name[20];
+    cin>>name;
+    cout<<"Your Name "<<name;
+}
